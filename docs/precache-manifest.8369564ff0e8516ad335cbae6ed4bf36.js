a0a471fde6e2f96eedba63ba8ab8bf93f3a2390c
Gleb Posobin <posobin@gmail.com>
2019-01-09 00:05:24 -0500

    Move to posobin.com

---
+++ docs/precache-manifest.8369564ff0e8516ad335cbae6ed4bf36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae53c2f0b50bd3cca20a",
+    "url": "/gifgif/static/css/main.4104b15e.chunk.css"
+  },
+  {
+    "revision": "ae53c2f0b50bd3cca20a",
+    "url": "/gifgif/static/js/main.ae53c2f0.chunk.js"
+  },
+  {
+    "revision": "c75ed60f03eec60fb363",
+    "url": "/gifgif/static/css/1.275a8628.chunk.css"
+  },
+  {
+    "revision": "c75ed60f03eec60fb363",
+    "url": "/gifgif/static/js/1.c75ed60f.chunk.js"
+  },
+  {
+    "revision": "e1f1c0dc6ca6bb5ffd15",
+    "url": "/gifgif/static/js/runtime~main.e1f1c0dc.js"
+  },
+  {
+    "revision": "1747954a71519c35253b573d045b973a",
+    "url": "/gifgif/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Royale-with-Cheese/precache-manifest.e6d7304342eac429e9263e7e598070ff.js"
+  "/gifgif/precache-manifest.8369564ff0e8516ad335cbae6ed4bf36.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Royale-with-Cheese/index.html", {
+workbox.routing.registerNavigationRoute("/gifgif/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
